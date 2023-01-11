82b20a9bf24bedb3156dd6cd598b3659e45600b1
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-01-11 17:48:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -51,37 +51,78 @@ $(function () {
 
     $('.mainContent .img_slide').slick({
         arrows: false,
-        vertical: true,
-        infinite: false,
+        fade: true,
+        autoplay: true,
+    });
+
+    var bgColor = ['#f6fdff', '#efefef', '#d0d9d5']
+    $('.main_content_case').css({ background: bgColor[0] })
+
+    $('.img_slide').on('init afterChange', function (e, s, c) {
+        // console.log(c);
+        $('.mainContent .text_box ul li').eq(c).addClass('on').siblings().removeClass('on');
+
+        $('.main_content_case').css({ background: bgColor[c] })
+    });
+    // main slide
+
+    $('.mainProduct .inner ul li').mouseenter(function () {
+        var change_image = $(this).attr('data-img')
+        $('.mainProduct .main_product_img').css({
+            'background-image': 'url(' + change_image + ')'
+        });
+
+
+        $(this).addClass('on').siblings().removeClass('on');
     });
 
     $(window).on('scroll', function () {
-
-    });
-
-    $('.main_content_case').on('wheel', function (e) {
-        if (e.originalEvent.deltaY < 0) {
-            $('.mainContent .img_slide').slick('slickPrev');
-        } else {
-            $('.mainContent .img_slide').slick('slickNext');
-        }
-    });
-
-    $('.main_content_case').on('mouseover', function () {
-        var thisTop = $(this).parent().offset().top;
         var sct = $(window).scrollTop();
-        console.log(thisTop, sct);
-        if (sct > thisTop) {
-        }
 
-        $(this).on('wheel', function (e) {
-            if (e.originalEvent.deltaY < 0) {
-                $('.mainContent .img_slide').slick('slickPrev');
-            } else {
-                $('.mainContent .img_slide').slick('slickNext');
-            }
-        });
+        if (sct > 1000) {
+            $('.to_top').addClass('on');
+        } else {
+            $('.to_top').removeClass('on');
+        }
+    });
+
+    $('.to_top').on('click', function () {
+        $('html, body').stop().animate({ scrollTop: 0 }, 600)
     })
+
+
+
+
+    // $('.mainProduct .inner ul li').mouseleave(function () {
+    //     $('.mainProduct .main_product_img').css({
+    //         'background-image': ''
+    //     })
+    // })
+
+
+    // $('.main_content_case').on('wheel', function (e) {
+    //     if (e.originalEvent.deltaY < 0) {
+    //         $('.mainContent .img_slide').slick('slickPrev');
+    //     } else {
+    //         $('.mainContent .img_slide').slick('slickNext');
+    //     }
+    // });
+
+    // $('.main_content_case').on('mouseover', function () {
+    //     var thisTop = $(this).parent().offset().top;
+    //     var sct = $(window).scrollTop();
+    //     console.log(thisTop, sct);
+    //     if (sct > thisTop) {
+    //     }
+
+    //     $(this).on('wheel', function (e) {
+    //         if (e.originalEvent.deltaY < 0) {
+    //             $('.mainContent .img_slide').slick('slickPrev');
+    //         } else {
+    //             $('.mainContent .img_slide').slick('slickNext');
+    //         }
+    //     });
+    // })
 
 
 })
